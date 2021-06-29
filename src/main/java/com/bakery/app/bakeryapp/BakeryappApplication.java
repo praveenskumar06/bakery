cd2307effbf305a564692fae6443c92a862d3087
praveen <praveenkumar@project.com>
2021-06-29 12:55:54 +0530

    refs #0 initial commit

---
+++ src/main/java/com/bakery/app/bakeryapp/BakeryappApplication.java
@@ -0,0 +1,13 @@
+package com.bakery.app.bakeryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakeryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakeryappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bakery/app/bakeryapp/BakeryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakery.app.bakeryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
